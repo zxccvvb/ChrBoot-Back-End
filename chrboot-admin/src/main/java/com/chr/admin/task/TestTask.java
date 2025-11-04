345a696883b00160fa21826b9201a3d0acd2eee7
chr <1977471418@qq.com>
2025-11-04 18:02:07 +0800

    feat: 在定时任务中添加websocket发送信息

--- chrboot-admin/src/main/java/com/chr/admin/task/TestTask.java
+++ chrboot-admin/src/main/java/com/chr/admin/task/TestTask.java
@@ -1,7 +1,9 @@
 package com.chr.admin.task;
 
 
+import com.chr.admin.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestTask {
 
-
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     @Scheduled(cron = "0/5 * * * * ?") //每5秒执行一次
     public void test1(){
         log.info("定时任务开始执行...");
+        webSocketServer.sendToAllClient("测试websocket");
+
     }
 
 
