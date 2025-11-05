ea86b4427f9a34fd6fcdb0651b7f030368f368d4
chr <1977471418@qq.com>
2025-11-05 14:29:46 +0800

    feat: 删除字典表准备使用枚举作为字典

--- chrboot-admin/src/main/resources/chrbootdatabase.sql
+++ chrboot-admin/src/main/resources/chrbootdatabase.sql
@@ -12,29 +12,3 @@ CREATE TABLE `chrbootdatabase`.`chr_user` (
   `create_user` bigint DEFAULT NULL COMMENT '创建人',
   `update_user` bigint DEFAULT NULL COMMENT '修改人'
 )COMMENT='用户表';
-
--- 字典类型表
-CREATE TABLE chr_dict_type (
-   `id`          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '字典类型id',
-   `name`        VARCHAR(32) NOT NULL UNIQUE COMMENT '类型名称',
-   `description` VARCHAR(128) NOT NULL COMMENT '中文说明',
-   `is_deleted`  BOOLEAN NOT NULL DEFAULT 0 COMMENT '逻辑删除字段'
-#    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-#    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-#    `create_user` bigint DEFAULT NULL COMMENT '创建人',
-#    `update_user` bigint DEFAULT NULL COMMENT '修改人'
-) COMMENT='字典类型';
-
--- 字典值表
-CREATE TABLE chr_dict_item (
-   `id`          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '字典值id',
-   `type_id`     bigint NOT NULL COMMENT '对应 chr_dict_type.id',
-   `value`       int NOT NULL COMMENT '值编码',
-   `label`       VARCHAR(64) NOT NULL COMMENT '显示文本',
-   `sort`        int NOT NULL DEFAULT 0,
-   `is_deleted`  BOOLEAN NOT NULL DEFAULT 0 COMMENT '逻辑删除字段'
-#    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-#    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-#    `create_user` bigint DEFAULT NULL COMMENT '创建人',
-#    `update_user` bigint DEFAULT NULL COMMENT '修改人'
-) COMMENT='字典值';
